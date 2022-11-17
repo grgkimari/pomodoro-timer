885b55992acffbf4a54c02676691c34d55d106cf
Kimari <grgkimari@gmail.com>
2022-11-18 01:39:22 +0300

    fixed bug in custom session length setting

--- pomodoro-timer/src/Components/ValueModifier.js
+++ pomodoro-timer/src/Components/ValueModifier.js
@@ -8,7 +8,7 @@ const ValueModifier = (props) => {
             <h5 className="valueName">{props.valueName}</h5>
             <button className="modButton increase" onClick={props.id === "session" ? (event) => props.clickHandler( {type : INCREMENT_SESSION_TIME}) : (event) => props.clickHandler( {type : INCREMENT_BREAK_TIME})}> + </button>
             <div className="value">{props.value}</div>
-            <button className="modButton reduce" onClick={(event) => props.clickHandler( {type : DECREMENT_BREAK_TIME})}> - </button>
+            <button className="modButton reduce" onClick={props.id === "session" ? (event) => props.clickHandler( {type : DECREMENT_SESSION_TIME}) : (event) => props.clickHandler( {type : DECREMENT_BREAK_TIME})}> - </button>
         </div>
     )
 }
