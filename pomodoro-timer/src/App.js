0faf414441f37320606e12cb126054fc07604967
sasha-lane <106698273+sasha-lane@users.noreply.github.com>
2022-11-25 23:52:37 +0300

    implemented countdown functionality

--- pomodoro-timer/src/App.js
+++ pomodoro-timer/src/App.js
@@ -1,23 +1,54 @@
 import './App.css';
-import ActionButton from './Components/ActionButton';
 import Timer from './Components/Timer';
 import ValueModifier from './Components/ValueModifier';
-import {useEffect, useReducer} from 'react'
-import mainReducer, {initialState, COUNT_DOWN, STOP_TIMER} from './reducers';
+import React, {useEffect, useReducer} from 'react'
+import ReactDOM from 'react-dom'
+import mainReducer, {initialState, TOGGLE_POWER, CLEAR_INTERVAL, SET_INTERVAL, COUNT_DOWN} from './reducers';
 
 
 
 function App() {
+  const[state, dispatch] = useReducer (mainReducer, initialState)
 
+  const toggle_timer = (event) => {
+    dispatch({
+      type: TOGGLE_POWER
+    })
+    console.log("Switched on")
+    if(event.target.innerHTML === "Start"){
+      event.target.innerHTML = "Stop"
+    }
+    else {
+      event.target.innerHTML = "Start"
+    }
+    console.log("Button Changed")
+      if(state.interval === null){
+        dispatch({
+          type: SET_INTERVAL,
+          payload: setInterval(() => dispatch({type : COUNT_DOWN}), 1000)
+        })
+        console.log("Interval set")
+      }
+    else{
+      clearInterval(state.interval)
+      dispatch({
+        type : CLEAR_INTERVAL
+      })
+      console.log("Interval cleared")
+    }
+  }
+  
+ 
+  console.log("State : " + JSON.stringify(state))
   return (
-
 
     <div className="App">
       <Timer time = {{minutes : state.time.minutes, seconds : state.time.seconds}} />
-      <ValueModifier valueName="Session Time" id="session" value={state.sessionTime}></ValueModifier>
-      <ValueModifier valueName ="Break Time" id="break" value={state.breakTime} ></ValueModifier>
-      <ActionButton actionName="Start / Stop" id="start" clickHandler={handleClick}></ActionButton>
-      <ActionButton actionName="Refresh" id="refresh"></ActionButton>
+      <ValueModifier valueName="Session Time" id="session" value={state.sessionTime} clickHandler = {dispatch}></ValueModifier>
+      <ValueModifier valueName ="Break Time" id="break" value={state.breakTime} clickHandler = {dispatch}></ValueModifier>
+      <button className="actionButton" id="start" onClick={toggle_timer}>
+            Start
+        </button>
     </div>
   );
 }
--- pomodoro-timer/src/reducers.js
+++ pomodoro-timer/src/reducers.js
@@ -2,12 +2,16 @@ export const INCREMENT_SESSION_TIME = "INCREMENT-SESSION-TIME"
 export const DECREMENT_SESSION_TIME = "DECREMENT-SESSION-TIME"
 export const INCREMENT_BREAK_TIME = "INCREMENT-BREAK-TIME"
 export const DECREMENT_BREAK_TIME = "DECREMENT-BREAK-TIME"
+export const SET_INTERVAL = "SET_INTERVAL"
 export const COUNT_DOWN = "COUNT-DOWN"
-export const STOP_TIMER = "STOP-TIMER"
+export const CLEAR_INTERVAL = "CLEAR-INTERVAL"
+export const TOGGLE_POWER = "TOGGLE-POWER"
 export const initialState = {
     running : false,
-    sessionTime : 25,
-    breakTime: 5,
+    interval : null,
+    sessionMode : true,
+    sessionTime : 1,
+    breakTime: 1,
     time : {
         minutes : 0,
         seconds : 5,
@@ -30,7 +34,64 @@ const mainReducer = (state = initialState, action) => {
                 return state
             }
             return Object.assign({}, state, {breakTime : state.breakTime - 1})
-
+        
+        case TOGGLE_POWER:
+            console.log("Changing running from " + state.running + " to " + !state.running)
+            return Object.assign({}, state, {running : !state.running})
+        
+        case COUNT_DOWN:
+            console.log("Countdown")
+            if(state.time.seconds > 0){
+                return Object.assign({}, state, {time : {
+                    minutes : state.time.minutes,
+                    seconds : state.time.seconds -  1,
+                }})
+            }
+            else{
+                if(state.time.minutes > 0){
+                    return Object.assign({}, state, {
+                        time : {
+                            minutes : state.time.minutes - 1,
+                            seconds : 59,
+                        }
+                    })
+                }
+                else{
+                    clearInterval(state.interval)
+                    console.log("___________________\n- - - DONE - - - \n ________________________")
+                    if(state.sessionMode){
+                        return Object.assign({}, state, {
+                            sessionMode : !state.sessionMode,
+                            interval : null,
+                            running : false,
+                            time : {
+                                minutes : state.breakTime,
+                                seconds : 0,
+                            }
+                        })
+                    }
+                    else {
+                        return Object.assign({}, state, {
+                            sessionMode : !state.sessionMode,
+                            interval : null,
+                            running : false,
+                            time : {
+                                minutes : state.sessionTime,
+                                seconds : 0,
+                            }
+                        })
+                        }
+                    }
+                }
+        
+        case SET_INTERVAL:
+            console.log("Setting interval to : " + action.payload.interval)
+            return Object.assign({}, state, {interval : action.payload.interval})
+        
+        case CLEAR_INTERVAL:
+            clearInterval(state.interval)
+            return Object.assign({}, state, {interval : null})
+        
         default:
             return Object.assign({}, initialState)
     }
